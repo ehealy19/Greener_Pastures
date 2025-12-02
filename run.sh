52c2c5d3d1be3ea649b276a08abc1bc609d3338d
Lizzie Healy <emh201@georgetown.edu>
2025-12-02 18:17:01 -0500

    fixing file paths in run.sh file

--- run.sh
+++ run.sh
@@ -4,20 +4,20 @@
 # =======================================================
 
 # convert addresses to lat/long
-chmod +x convert_to_geo.py
-python convert_to_geo.py
+chmod +x ../scripts/convert_to_geo.py
+python ../scripts/convert_to_geo.py
 
 # run set-up file first 
 chmod +x set-up.sh
 ./set-up.sh
 
 # run the Python fie
-python vroom.py
+python ../scripts/vroom.py
 
 # clean-up
 chmod +x clean-up.sh
 ./clean-up.sh
 
 # convert lat/long back to addreses
-chmod +x convert_to_add.py
-python convert_to_add.py
+chmod +x ../scripts/convert_to_add.py
+python ../scripts/convert_to_add.py
